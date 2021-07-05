b1a838a5edcfc4cd11473f5d89b22d00a3f332e7
NGUYEN THANH VIET <ntviet2409@gmail.com>
2021-07-05 12:36:00 +0000

    Maven artifacts for 1.0-SNAPSHOT

--- src/main/java/com/pageObjects/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.pageObjects;
-
-import org.apache.commons.lang3.RandomStringUtils;
-
-public class Constants {
-    public static final String RANDOM = RandomStringUtils.randomAlphabetic(5);
-}
